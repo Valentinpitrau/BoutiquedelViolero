5715dadf6e1b03ea85da7e047be56e38ead1cb02
Valen <valentinpitrau@gmail.com>
2022-10-25 19:40:25 -0300

    commit1

--- src/components/ItemList/ItemListContainer.js
+++ src/components/ItemList/ItemListContainer.js
@@ -2,31 +2,35 @@
 import Container from 'react-bootstrap/Container';
 import { useEffect, useState } from 'react';
 import Itemlist from './ItemList';
-import {useParams} from 'react-router-dom';
-import {getProducts} from '../../utils/products';
+import { useParams } from 'react-router-dom';
+import {getAllProducts, getProductsByCategory, getProduct} from '../../utils/products';
 import './ItemListContainer.css';
 
+
 const ItemListContainer = ({ greeting }) => {
-    const {categoryName} = useParams();
-    const [products, setProducts] = useState([]);
+  const { categoryName } = useParams();
+  const [products, setProducts] = useState([]);
 
-    useEffect(() => {
-        console.log (categoryName);
-    }, [categoryName])
 
-    useEffect(()=> {
-        getProducts()
-        .then ((data) => setProducts(products))
-        .catch((error) => console.warn(error))
-    }, [])
 
-    return (
-        <Container>
-            <h1>Productos</h1>
-            <h3 className="greeting">{greeting}</h3>
-            <Itemlist products={products}/>
-        </Container>
-    );
+  const getProducts = (categoryName) => {
+    if (categoryName != null) {
+      getProductsByCategory(categoryName);
+      return
+    }
+    getAllProducts();
+  }
+
+  useEffect 
+ 
+  return (
+    <Container>
+      <h1>Productos</h1>
+      <h3 className="greeting">{greeting}</h3>
+      <Itemlist products={products} />
+    </Container>
+  );
 }
+
 
 export default ItemListContainer;
