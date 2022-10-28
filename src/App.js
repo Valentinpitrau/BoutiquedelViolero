0770191f90154afbaed0299ce0745182ba499282
Valen <valentinpitrau@gmail.com>
2022-10-28 12:03:02 -0300

    Commit 11

--- src/App.js
+++ src/App.js
@@ -1,24 +1,32 @@
 
 import './App.css';
-import NavBar from './components/NavBar';
+import NavBar from '../src/components/NavBar';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import {BrowserRouter, Routes, Route} from 'react-router-dom';
-import ItemDetailContainer from './components/ItemDetailContainer';
+import ItemDetailContainer from './components/ItemDetail/ItemDetailContainer';
 import Cart from './components/Cart/Cart';
 import Checkout from './components/Checkout';
 import ItemListContainer from './components/ItemList/ItemListContainer';
+import { useEffect } from 'react';
+import {doc, getFirestore, getDoc, collection, getDocs} from 'firebase/firestore';
+import { CartProvider } from './context/CartContext';
+
 
 function App() {
+
+
   return (
     <BrowserRouter>
+    <CartProvider>
     <NavBar />
     <Routes>
       <Route path='/' element={<ItemListContainer greeting={'Brienvenidos a mi tienda'} />} />
-      <Route path='/category/:categoryName' element={<ItemListContainer greeting={'Brienvenidos a mi tienda'} />} />
+      <Route path='/category/:categoryId' element={<ItemListContainer greeting={'Brienvenidos a mi tienda'} />} />
       <Route path='/item/:id' element={<ItemDetailContainer />} />
       <Route path='/cart' element={<Cart />} />
       <Route path='/checkout' element={<Checkout/>} />
     </Routes>
+    </CartProvider>
     </BrowserRouter>
     
   );
